98031d9d6863435c82c3ccb7e89e6f2fceabf616
Steve Grard <grard.steve@gmail.com>
2023-04-28 11:18:33 +0200

    Cleaning of the new code and delete of savedTasks.js

--- assets/js/modules/addTask.js
+++ assets/js/modules/addTask.js
@@ -7,14 +7,10 @@ export function addTask(){
     const taskCompletion = document.getElementById("task-completion")
     const taskDeadline = document.getElementById("task-deadline")
 
-    let test = []
-
     if(taskTitle.value == "" || taskInformations.value == "" || taskDeadline == null){
         alert("You must enter all informations of your task !")
     }else{
         const oldTasks = JSON.parse(window.localStorage.getItem("tasks")) || []
-        console.log(oldTasks)
-
         const newTask = {
             id : idRandom(),
             "title" : taskTitle.value,
@@ -22,25 +18,8 @@ export function addTask(){
             completion : taskCompletion.checked,
             deadline : taskDeadline.value
         }
-
         oldTasks.push(newTask);
         localStorage.setItem('tasks', JSON.stringify(oldTasks))
-        console.log(localStorage)
-       
-
-        // tasksExistants.push({
-        //     // id : tasksExistants.length,
-        //     "title" : taskTitle.value,
-        //     "informations" : taskInformations.value,
-        //     "completion" : taskCompletion.checked,
-        //     "deadline" : taskDeadline.value
-        // })
-        // tasksExistants.push([tasksExistants.length, taskTitle.value, taskInformations.value, taskCompletion.checked, taskDeadline.value])
-        // let taskToStorage = JSON.stringify(newTask)
-        // tasksExistants.push(newTask)
-        // localStorage.setItem("tasksExistants", tasksExistants)
-        // Render the result
-        // showTasks()
     }
 }
 
